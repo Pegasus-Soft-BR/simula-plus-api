fa910cedfb42e12f90cccb06f9afeb73c3707fd9
Raffaello Damgaard <raffaello_neves@carrefour.com>
2025-09-17 19:16:16 -0300

    Não precisava desabilitar o seed.

--- src/Api/Configuration/DatabaseConfiguration.cs
+++ src/Api/Configuration/DatabaseConfiguration.cs
@@ -38,7 +38,7 @@ public static class DatabaseConfiguration
         else ctx.Database.Migrate();
 
         var seeder = new DatabaseSeeder(ctx);
-        // seeder.Seed();
+        seeder.Seed();
 
         return services;
     }
